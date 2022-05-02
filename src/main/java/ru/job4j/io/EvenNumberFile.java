8f0ed508a13f98c7d06f8601091e160ea388a52c
Zolotukhin Vladimir <daco.inc.v@gmail.com>
2022-05-02 10:20:49 +0400

    [#4898]: - implemented logic for file reading and checking number is even or not;

--- src/main/java/ru/job4j/io/EvenNumberFile.java
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -5,13 +5,23 @@ import java.io.IOException;
 
 public class EvenNumberFile {
     public static void main(String[] args) {
-        try (FileInputStream in = new FileInputStream("input.txt")) {
+        try (FileInputStream in = new FileInputStream("even.txt")) {
             StringBuilder text = new StringBuilder();
             int read;
             while ((read = in.read()) != -1) {
                 text.append((char) read);
             }
-            System.out.println(text);
+            String[] lines = text.toString().split(System.lineSeparator());
+            for (String line : lines) {
+                StringBuilder lineBuilder = new StringBuilder();
+                lineBuilder.append(line);
+                if (Integer.parseInt(line) % 2 == 0) {
+                    lineBuilder.append(" - even number");
+                } else {
+                    lineBuilder.append(" - odd number");
+                }
+                System.out.println(lineBuilder);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
