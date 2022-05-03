a8064f6ec84a2ee9907051dee6cb570ee6307d4a
Zolotukhin Vladimir <daco.inc.v@gmail.com>
2022-05-03 14:13:38 +0400

    [#858]: - implemented logic for parsing config file; - tests have been added;

---
+++ src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,57 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Config {
+    private final String path;
+    private final Map<String, String> values = new HashMap<>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(this.path))) {
+            reader.lines()
+                    .filter(line -> !(line.isBlank() || line.startsWith("#")))
+                    .forEach(line -> {
+                        int index = line.indexOf("=");
+                        if (line.split("=").length <= 2 && index == 0) {
+                            throw new IllegalArgumentException();
+                        }
+                        values.put(
+                                line.substring(0, index),
+                                line.substring(index + 1)
+                        );
+                    });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        return values.get(key);
+    }
+
+    public int getSizeValues() {
+        return values.size();
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Config("app.properties"));
+    }
+}
---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,46 @@
+package ru.job4j.io;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ConfigTest {
+    @Test
+    public void whenPairWithoutComment() {
+        String path = "./wocomment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("hibernate.connection.username"), is("postgres"));
+        assertThat(config.value("hibernate.connection.password"), is("password"));
+    }
+
+    @Test
+    public void whenPairWithComment() {
+        String path = "./app.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("hibernate.connection.username"), is("postgres"));
+        assertThat(config.value("hibernate.connection.password"), is("password"));
+        assertThat(config.value("hibernate.connection.driver_class"), is("org.postgresql.Driver"));
+        assertThat(config.value("hibernate.connection.url"), is("jdbc:postgresql://127.0.0.1:5432/trackstudio"));
+        assertThat(config.value("hibernate.dialect"), is("org.hibernate.dialect.PostgreSQLDialect"));
+        assertThat(config.value("t"), is("t"));
+        assertThat(config.value("surname"), is(Matchers.nullValue()));
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void whenPairWithBreakInstance() {
+        String path = "./illegalarg.properties";
+        Config config = new Config(path);
+        config.load();
+    }
+
+    @Test
+    public void whenPairWithCommentAndEmptyLine() {
+        String path = "./wcommentandemptyln.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.getSizeValues(), is(6));
+    }
+}
