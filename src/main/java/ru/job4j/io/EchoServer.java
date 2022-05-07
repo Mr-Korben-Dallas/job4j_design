095ee34897d61720064c2b543686d31fbe7f8554
Zolotukhin Vladimir <daco.inc.v@gmail.com>
2022-05-07 15:32:26 +0400

    [#268853]: - exception added to log;

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -3,8 +3,12 @@ package ru.job4j.io;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class EchoServer {
+    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
+
     private static String prepareAnswer(BufferedReader in, ServerSocket server) throws IOException {
         String answer = "";
         for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
@@ -25,7 +29,7 @@ public class EchoServer {
         return answer;
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
@@ -38,6 +42,8 @@ public class EchoServer {
                     out.flush();
                 }
             }
+        } catch (IOException e) {
+            LOG.error("IOException", e);
         }
     }
 }
