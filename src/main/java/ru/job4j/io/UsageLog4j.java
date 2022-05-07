c3bf9418a0636d07d4de37f2617c281e499b8f6b
Zolotukhin Vladimir <daco.inc.v@gmail.com>
2022-05-07 15:26:19 +0400

    [#268852]: - added output;

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -7,10 +7,15 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        byte a = 1;
+        short b = 1;
+        int c = 1;
+        long d = 1;
+        char e = 'A';
+        float f = 1;
+        double g = 1;
+        boolean h = false;
+
+        LOG.debug("a : {}, b : {}, c : {}, d : {}, e : {}, f : {}, g : {}, h : {}", a, b, c, d, f, g, e, h);
     }
 }
