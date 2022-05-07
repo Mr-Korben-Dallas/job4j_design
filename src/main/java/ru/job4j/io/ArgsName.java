08a8629d7da89ec23c351b4e153c75e017143e42
Zolotukhin Vladimir <daco.inc.v@gmail.com>
2022-05-07 12:06:16 +0400

    [#295518 #297974]: - refactoring in validation logic;

--- src/main/java/ru/job4j/io/ArgsName.java
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -7,7 +7,7 @@ public class ArgsName {
     private final Map<String, String> values = new HashMap<>();
 
     public String get(String key) {
-        validateKeyValues(key);
+        checkIsKeyExists(key);
         return values.get(key);
     }
 
@@ -16,8 +16,8 @@ public class ArgsName {
             String[] content = argValue.split("=", 2);
             String key = content[0].substring(1);
             String value = content[1];
+            validateKeyValueNotEmpty(key, value);
             values.put(key, value);
-            validateKeyValues(key);
         }
     }
 
@@ -29,14 +29,26 @@ public class ArgsName {
     }
 
     private static void validateInput(String[] args) {
-        if (args.length == 0) {
+        if (args.length != 0) {
+            for (String arg : args) {
+                if (!arg.startsWith("-")) {
+                    throw new IllegalArgumentException("You entered the argument name incorrectly.");
+                }
+            }
+        } else {
             throw new IllegalArgumentException("You need enter at least one argument.");
         }
     }
 
-    private void validateKeyValues(String key) {
-        if (!values.containsKey(key) || values.get(key).isBlank()) {
+    private void checkIsKeyExists(String key) {
+        if (!values.containsKey(key)) {
             throw new IllegalArgumentException("You entered the argument name incorrectly, or it does not exist.");
+        }
+    }
+
+    private void validateKeyValueNotEmpty(String key, String value) {
+        if (key.isEmpty() || value.isEmpty()) {
+            throw new IllegalArgumentException("You entered the argument name incorrectly.");
         }
     }
 
