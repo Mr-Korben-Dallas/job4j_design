2798c9af2a4e57217edcb71176d243cf485aa858
Zolotukhin Vladimir <daco.inc.v@gmail.com>
2022-05-07 14:53:02 +0400

    [#7921]: - implemented simple bot via client-server app;

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -5,6 +5,26 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
+    private static String prepareAnswer(BufferedReader in, ServerSocket server) throws IOException {
+        String answer = "";
+        for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
+            if (str.contains("msg=")) {
+                String param = str.split("msg=")[1].split(" ")[0];
+                if (param.equals("Exit")) {
+                    answer = "Bye";
+                    server.close();
+                }
+                if (param.equals("Hello")) {
+                    answer = "Hello";
+                }
+                if (param.equals("Any")) {
+                    answer = "What?";
+                }
+            }
+        }
+        return answer;
+    }
+
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
@@ -13,14 +33,8 @@ public class EchoServer {
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
-                        if (str.contains("msg=")) {
-                            String param = str.split("msg=")[1].split(" ")[0];
-                            if (param.equals("Bye")) {
-                                server.close();
-                            }
-                        }
-                    }
+                    String answer = prepareAnswer(in, server);
+                    out.write(answer.getBytes());
                     out.flush();
                 }
             }
