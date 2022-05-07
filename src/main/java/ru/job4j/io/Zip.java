437329260f5909025be1a9433d5d02c42d1be503
Zolotukhin Vladimir <daco.inc.v@gmail.com>
2022-05-07 12:59:53 +0400

    [#861]: - implemented logic for zip files in the directory;

---
+++ src/main/java/ru/job4j/io/Zip.java
@@ -0,0 +1,48 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class Zip {
+    public void packFiles(List<File> sources, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            for (File source : sources) {
+                zip.putNextEntry(new ZipEntry(source.getPath()));
+                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
+                    zip.write(out.readAllBytes());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void packSingleFile(File source, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            zip.putNextEntry(new ZipEntry(source.getPath()));
+            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
+                zip.write(out.readAllBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        ArgsName argsName = ArgsName.of(args);
+        Path path = Paths.get(argsName.get("d"));
+        Predicate<Path> excludeFiles = p -> !p.toFile().getName().endsWith(argsName.get("e"));
+        try {
+            List<File> sources = Search.search(path, excludeFiles).stream().map(Path::toFile).collect(Collectors.toList());
+            new Zip().packFiles(sources, Paths.get(argsName.get("o")).toFile());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
